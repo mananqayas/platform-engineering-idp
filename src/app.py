bca5f3eb142141a6c5ac1c0fd66053d8436b5520
digitalifylabs <=>
2026-06-07 11:41:29 -0400

    added v6

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'ArgoCD deployment v5'
+        'message': 'ArgoCD deployment v6'
     })
 @app.route("/api/v1/healthz")
 def health():
