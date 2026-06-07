93e1860f75b1bf5da87d053e554a8a90cfe67742
digitalifylabs <=>
2026-06-07 14:44:26 -0400

    new release

--- src/app.py
+++ src/app.py
@@ -13,6 +13,15 @@ def details():
         'message': 'ArgoCD deployment'
     })
 @app.route("/api/v1/healthz")
+@app.route("/api/v1/info")
+def details():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        'hostname': socket.gethostname(),
+        'message': 'You are doing great, little human! <3',
+        'deployed_on': 'kubernetes'
+    })
+@app.route("/api/v1/healthz")
 def health():
     return jsonify({
         'status': 'up'
